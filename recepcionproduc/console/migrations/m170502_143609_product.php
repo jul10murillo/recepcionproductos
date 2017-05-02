ed044cdbe3b6c04234d85956d4e07cdb0c10dc0e
Julio Murillo <Julio Murillo@localhost>
2017-05-02 10:38:27 -0400

    Iniciando

---
+++ recepcionproduc/console/migrations/m170502_143609_product.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Migration;
+
+class m170502_143609_product extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+        
+    }
+
+    public function down()
+    {
+        echo "m170502_143609_product cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
