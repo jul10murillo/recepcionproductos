ac520606ed493f6b40856eb88113cb58003b7343
Julio Murillo <Julio Murillo@localhost>
2017-06-13 14:30:52 -0400

    agregando Test

---
+++ recepcionproduc/frontend/tests/unit/MappingTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace frontend\tests;
+
+
+class MappingTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \frontend\tests\UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testMe()
+    {
+
+    }
+}
